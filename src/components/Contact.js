dd073d04d7815e154f67b6c390368a8fa95ab88a
Manuel Mayo <71531146+MANUEL12-pixel@users.noreply.github.com>
2021-10-02 21:04:26 -0500

    sendEmail

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,158 +2,184 @@ import React from "react";
 import "../styles/Contact.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import ReCAPTCHA from "react-google-recaptcha";
-import { useForm } from "react-hook-form";
+// import { useForm } from "react-hook-form";
 import axios from "axios";
 
-function Contact() {
-  /*
-  const [setPost] = React.useState(null);
-  const baseURL = "http://localhost:3500/form";
+export default class Contact extends React.Component {
+  // const {
+  //   register,
+  //    formState: { errors },
+  //  } = useForm();
 
-   React.useEffect(() => {
-     axios.get(`${baseURL}`).then((response) => {
-       setPost(response.data);
-     });
-   }, []);
+  // const onSubmit = (data) => console.log(data);
 
-  function SubmitAP() {
-    axios
-      .post(baseURL, {
-        Names: "",
-        Asunt: "",
-        email: "",
-        Message: "",
-      })
-      .then((response) => {
-        setPost(response.data);
-      });
+  name = React.createRef();
+  asunt = React.createRef();
+  email = React.createRef();
+  message = React.createRef();
+
+  state = {
+    name: "",
+    asunt: "",
+    email: "",
+    message: "",
+  };
+
+  comprobarCambios = () => {
+    //actualiza el state para cada cambio
+    var name = this.name.current.value;
+    var asunt = this.asunt.current.value;
+    var email = this.email.current.value;
+    var message = this.message.current.value;
+
+    this.setState({
+      name: name,
+      asunt: asunt,
+      email: email,
+      message: message,
+    });
+  };
+
+  constructor() {
+    //hace el bind el metodo
+    super();
+    this.sendEmail = this.sendEmail.bind(this);
   }
-*/
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm();
-  const onSubmit = (data) => console.log(data);
 
-  console.log(watch("example"));
-
-  function onChange(value) {
-    console.log("Captcha value:", value);
+  async sendEmail(e) {
+    //metodo para enviar el email
+    e.preventDefault();
+    const { name, email, asunt, message } = this.state;
+    const form = await axios.post("/api/form", {
+      name,
+      asunt,
+      email,
+      message,
+    });
   }
 
-  return (
-    <div className="Contactfond">
-      <div className="Contact">
-        <h1 className="titleContact">Contact</h1>
-      </div>
-      <div className="container">
-        <div className="wrapper wrapperContact">
-          <form onSubmit={handleSubmit(onSubmit)} method="post">
-            <div className="row rowContact">
-              <div className="col">
-                <input
-                  type="text"
-                  class="form-control"
-                  placeholder="Nombres"
-                  aria-label="Names"
-                  {...register("Names", {
-                    required: "Ingresa tu nombre",
-                    minLength: 10,
-                    maxlength: 100,
-                  })}
-                />
-                {errors.Names && (
+  render() {
+    return (
+      <div className="Contactfond">
+        <div className="Contact">
+          <h1 className="titleContact">Contact</h1>
+        </div>
+        <div className="container">
+          <div className="wrapper wrapperContact">
+            <form className="formulario" onSubmit={this.sendEmail}>
+              <div className="row rowContact">
+                <div className="col">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Nombres"
+                    aria-label="Names"
+                    name="name"
+                    onChange={this.comprobarCambios}
+                    ref={this.name}
+                    // {...register("Names", {
+                    //   required: "Ingresa tu nombre",
+                    //   minLength: 10,
+                    //   maxlength: 100,
+                    // })}
+                  />
+                  {/* {errors.Names && (
                   <span>
                     <FontAwesomeIcon icon={["fas", "exclamation-triangle"]} />{" "}
                     {errors.Names.message}
                   </span>
-                )}
-              </div>
-              <div className="col">
-                <input
-                  type="text"
-                  class="form-control"
-                  placeholder="Asunto"
-                  aria-label="Asunt"
-                  {...register("Asunt", {
-                    required: "Por favor ingresa un asunto",
-                    minLength: 10,
-                    maxlength: 50,
-                  })}
-                />
-                {errors.Asunt && (
+                )} */}
+                </div>
+                <div className="col">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Asunto"
+                    aria-label="Asunt"
+                    name="asunt"
+                    onChange={this.comprobarCambios}
+                    ref={this.asunt}
+                    // {...register("Asunt", {
+                    //   required: "Por favor ingresa un asunto",
+                    //   minLength: 10,
+                    //   maxlength: 50,
+                    // })}
+                  />
+                  {/* {errors.Asunt && (
                   <span>
                     <FontAwesomeIcon icon={["fas", "exclamation-triangle"]} />{" "}
                     {errors.Asunt.message}
                   </span>
-                )}
-              </div>
-              <div className="col">
-                <input
-                  type="email"
-                  class="form-control"
-                  id="inputEmail4"
-                  placeholder="E-mail"
-                  {...register("email", {
-                    required: "Ingrese su correo",
-                    pattern: {
-                      value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                      message: "Ingresa un correo valido",
-                    },
-                  })}
-                />
-                {errors.email && (
+                )} */}
+                </div>
+                <div className="col">
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="inputEmail4"
+                    placeholder="E-mail"
+                    name="email"
+                    onChange={this.comprobarCambios}
+                    ref={this.email}
+                    // {...register("email", {
+                    //   required: "Ingrese su correo",
+                    //   pattern: {
+                    //     value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                    //     message: "Ingresa un correo valido",
+                    //   },
+                    // })}
+                  />
+                  {/* {errors.email && (
                   <span>
                     <FontAwesomeIcon icon={["fas", "exclamation-triangle"]} />{" "}
                     {errors.email.message}
                   </span>
-                )}
-              </div>
-              <div className="col">
-                <textarea
-                  class="form-control"
-                  id="exampleFormControlTextarea1"
-                  rows="3"
-                  placeholder="Mensaje"
-                  {...register("Message", {
-                    required: "Ingresa un mensaje",
-                    minLength: 10,
-                    maxlength: 200,
-                  })}
-                ></textarea>
-                {errors.Message && (
+                )} */}
+                </div>
+                <div className="col">
+                  <textarea
+                    className="form-control"
+                    id="exampleFormControlTextarea1"
+                    rows="3"
+                    placeholder="Mensaje"
+                    name="message"
+                    onChange={this.comprobarCambios}
+                    ref={this.message}
+                    // {...register("Message", {
+                    //   required: "Ingresa un mensaje",
+                    //   minLength: 10,
+                    //   maxlength: 200,
+                    // })}
+                  ></textarea>
+                  {/* {errors.Message && (
                   <span>
                     <FontAwesomeIcon icon={["fas", "exclamation-triangle"]} />{" "}
                     {errors.Message.message}
                   </span>
-                )}
+                )} */}
+                </div>
+                <div className="col">
+                  <ReCAPTCHA
+                    className="recap"
+                    sitekey="6LdpF4UcAAAAABR79qJu-jGqkCrKpPA-SaEJ4dqt"
+                  />
+                  <button
+                    type="submit"
+                    className="btn btn-outline-light buttonSubmit"
+                    // onClick={SubmitAP}
+                  >
+                    <FontAwesomeIcon
+                      icon={["fas", "paper-plane"]}
+                      className="Iconsubmitbutton"
+                    />{" "}
+                    Enviar
+                  </button>
+                </div>
               </div>
-              <div className="col">
-                <ReCAPTCHA
-                  className="recap"
-                  sitekey="6LdpF4UcAAAAABR79qJu-jGqkCrKpPA-SaEJ4dqt"
-                  onChange={onChange}
-                />
-                <button
-                  type="submit"
-                  class="btn btn-outline-light buttonSubmit"
-                  // onClick={SubmitAP}
-                >
-                  <FontAwesomeIcon
-                    icon={["fas", "paper-plane"]}
-                    className="Iconsubmitbutton"
-                  />{" "}
-                  Enviar
-                </button>
-              </div>
-            </div>
-          </form>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
-export default Contact;
